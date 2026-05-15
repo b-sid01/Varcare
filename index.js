91ab9ede034370d708e06ef5ddbc3e5e7e93ade8
B Siddanth <siddu362003@gmail.com>
2026-05-15 08:02:31 +0000

    added CORS fix for confirm endpoint

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
